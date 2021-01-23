b2f95a28ddcf999a59dd27e3511453196b0e90f4
TimVargo <tim.g.vargo@gmail.com>
2021-01-23 12:30:40 -0700

    Driving implementation, but 'AngleUnit' is not resolvable

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveRectangleWithEncoder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveRectangleWithEncoder.java
@@ -21,6 +21,8 @@ import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
 import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
@@ -368,7 +370,7 @@ public class DriveRectangleWithEncoder extends LinearOpMode
 
             // Turn cw 90 degrees
             // We are in a corner of the rectangle, insert method call here to turn the corner
-
+            rotate(-90, drivepower);
 
             // Rectangle side cw2
 
@@ -400,7 +402,7 @@ public class DriveRectangleWithEncoder extends LinearOpMode
 
             // Turn cw 90 degrees
             // We are in a corner of the rectangle, insert method call here to turn the corner
-
+            rotate(-90, drivepower);
 
             // Rectangle side cw3
 
@@ -432,7 +434,7 @@ public class DriveRectangleWithEncoder extends LinearOpMode
 
             // Turn cw 90 degrees
             // We are in a corner of the rectangle, insert method call here to turn the corner
-
+            rotate(-90, drivepower);
 
             // Rectangle side cw4
 
@@ -464,8 +466,7 @@ public class DriveRectangleWithEncoder extends LinearOpMode
 
             // Turn cw 180 degrees
             // We are in the starting corner of the rectangle, insert method call here to turn completely around
-
-
+            rotate(180, drivepower);
         }
 
         // Disable Tracking when OpMode is complete;
@@ -528,4 +529,100 @@ public class DriveRectangleWithEncoder extends LinearOpMode
         dashboard.sendTelemetryPacket(vuforiapacket);
         idle();
     }
+
+    // reset the cumulative angle tracking to zero
+    private void resetAngle()
+    {
+        lastAngles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        globalAngle = 0;
+    }
+
+    // get current cumulative angle rotation from last reset
+    // @return Angle in degrees. + = left, - = right
+    private double getAngle()
+    {
+        // We experimentally determined the Z axis is the axis we want to use for heading angle.
+        // We have to process the angle because the imu works in euler angles so the Z axis is
+        // returned as 0 to +180 or 0 to -180 rolling back to -179 or +179 when rotation passes
+        // 180 degrees. We detect this transition and track the total cumulative angle of rotation.
+
+        Orientation angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+
+        double deltaAngle = angles.firstAngle - lastAngles.firstAngle;
+
+        if (deltaAngle < -180)
+            deltaAngle += 360;
+        else if (deltaAngle > 180)
+            deltaAngle -= 360;
+
+        globalAngle += deltaAngle;
+        lastAngles = angles;
+        return globalAngle;
+    }
+
+     // See if we are moving in a straight line and if not return a power correction value.
+     //  @return Power adjustment, + is adjust left - is adjust right
+    private double checkDirection()
+    {
+        // The gain value determines how sensitive the correction is to direction changes.
+        // You will have to experiment with your robot to get small smooth direction changes
+        // to stay on a straight line.
+        double correction, angle, gain = .10;
+        angle = getAngle();
+        if (angle == 0)
+            correction = 0;             // no adjustment.
+        else
+            correction = -angle;        // reverse sign of angle for correction.
+        correction = correction * gain;
+        return correction;
+    }
+
+     // rotate left or right the number of degrees. Does not support turning more than 180 degrees
+     //  @param degrees Degrees to turn, + is left - is right
+    private void rotate(int degrees, double power)
+    {
+        double  leftPower, rightPower;
+        // restart imu movement tracking.
+        resetAngle();
+
+        // getAngle() returns + when rotating counter clockwise (left) and - when rotating
+        // clockwise (right).
+
+        if (degrees < 0)
+        {   // turn right.
+            leftPower = power;
+            rightPower = -power;
+        }
+        else if (degrees > 0)
+        {   // turn left.
+            leftPower = -power;
+            rightPower = power;
+        }
+        else return;
+
+        // set power to rotate.
+        leftMotor.setPower(leftPower);
+        rightMotor.setPower(rightPower);
+
+        // rotate until turn is completed.
+        if (degrees < 0)
+        {
+            // On right turn we have to get off zero first.
+            while (opModeIsActive() && getAngle() == 0) {}
+            while (opModeIsActive() && getAngle() > degrees) {}
+        }
+        else    // left turn.
+            while (opModeIsActive() && getAngle() < degrees) {}
+
+        // turn the motors off.
+        rightMotor.setPower(0);
+        leftMotor.setPower(0);
+
+        // wait for rotation to stop.
+        sleep(1000);
+
+        // reset angle tracking on new heading.
+        resetAngle();
+    }
+
 }
