1dfe9c0338e61e7e9fab61da4f44d354dc52a686
Ebeida <hmebedia@gmail.com>
2021-01-30 14:40:26 -0700

    Moved over blocks code to Android Studeo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Ultimate_Goal_TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Ultimate_Goal_TeleOp.java
@@ -16,10 +16,12 @@ public class Ultimate_Goal_TeleOp extends LinearOpMode{
     DcMotorEx shooter;
     DcMotorEx intake;
     DcMotorEx roller;
+    DcMotorEx arm;
 
     // declare our servos
     Servo gate;
     Servo feeder;
+    Servo grabber;
 
     // called when init button is pressed
     @Override
@@ -30,8 +32,10 @@ public class Ultimate_Goal_TeleOp extends LinearOpMode{
         shooter = hardwareMap.get(DcMotorEx.class, "Shooter");
         intake = hardwareMap.get(DcMotorEx.class, "Intake");
         roller = hardwareMap.get(DcMotorEx.class, "Roller");
+        arm = hardwareMap.get(DcMotorEx.class, "Arm");
         gate = hardwareMap.get(Servo.class, "Gate");
         feeder = hardwareMap.get(Servo.class, "Feeder");
+        grabber = hardwareMap.get(Servo.class, "Grabber");
 
         // The motor on one side must be reversed to drive forward
         // Reverse the motor that runs backwards when connected directly to the battery
@@ -46,7 +50,6 @@ public class Ultimate_Goal_TeleOp extends LinearOpMode{
         while (opModeIsActive()) {
             rightMotor.setPower(gamepad1.right_stick_y);
             leftMotor.setPower(gamepad1.left_stick_y);
-            middleMotor.setPower(gamepad1.left_stick_x);
 
             if (gamepad2.x) {
                 if (shooter.getPower() == 0.0) {
@@ -56,6 +59,21 @@ public class Ultimate_Goal_TeleOp extends LinearOpMode{
                 }
             }
 
+            arm.setPower(0.5 * gamepad2.left_stick_y);
+            intake.setPower(gamepad2.right_stick_y);
+            roller.setPower(gamepad2.left_stick_x);
+
+            if (gamepad2.right_bumper) {
+                feeder.setPosition(0.45);
+            } else {
+                feeder.setPosition(0.73);
+            }
+
+            if (gamepad2.left_bumper) {
+                grabber.setPosition(0);
+            } else {
+                grabber.setPosition(0.3);
+            }
         }
     }
 }
