c1f5ee54d973cd803f7d74d51abf6197545dd612
AyazIsl123 <88250240+AyazIsl123@users.noreply.github.com>
2021-09-26 16:48:19 -0500

    Mecanum_TeleOp

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum_TeleOp2.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+@TeleOp(name="Mecaunum_TeleOp2")
+public class Mecanum_TeleOp2 {
+    //
+    private DcMotor Frontright;
+    private DcMotor Frontleft;
+    private DcMotor Backright;
+    private DcMotor Backleft;
+
+}
