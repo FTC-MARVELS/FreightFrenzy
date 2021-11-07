65a1e799a0892d9711e7075af3f452e8fe1c165d
Gazala <gazala_j_memon@yahoo.com>
2021-11-07 17:25:36 -0600

    Commit Mas Changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MAS_Auto_RedCarousel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MAS_Auto_RedCarousel.java
@@ -4,10 +4,10 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-import org.firstinspires.ftc.teamcode.RobotObjects.MAS.Claw;
+//import org.firstinspires.ftc.teamcode.RobotObjects.MAS.Claw;
 import org.firstinspires.ftc.teamcode.RobotObjects.Mecanum_Wheels;
 import org.firstinspires.ftc.teamcode.RobotObjects.Spinner;
-import org.firstinspires.ftc.teamcode.RobotObjects.Scanner;
+//import org.firstinspires.ftc.teamcode.RobotObjects.Scanner;
 
 /*
     Near Carousel
@@ -38,16 +38,15 @@ import org.firstinspires.ftc.teamcode.RobotObjects.Scanner;
 
 
 @Autonomous(name="MAS_Auto_RedCarousel")
-@Disabled
 public class MAS_Auto_RedCarousel extends LinearOpMode {
     //Configuration used: 6wheelConfig
     @Override
     public void runOpMode() throws InterruptedException {
         double speed = 0.2;
         Mecanum_Wheels mecanum = new Mecanum_Wheels(hardwareMap);
-        Claw claw = new Claw(hardwareMap);
+        //Claw claw = new Claw(hardwareMap);
         Spinner spinner = new Spinner(hardwareMap);
-        Scanner scanner = new Scanner(hardwareMap);
+       // Scanner scanner = new Scanner(hardwareMap);
         mecanum.IsAutonomous = true;
         mecanum.velocity = 400;
         mecanum.telemetry = this.telemetry;
@@ -58,13 +57,21 @@ public class MAS_Auto_RedCarousel extends LinearOpMode {
         waitForStart();
 
         // move right to go to spinner - 5 inches for eg
-        double spinnerDistance = 5;
-        mecanum.encoderDrive(speed,-spinnerDistance,0,spinnerDistance,spinnerDistance,0,-spinnerDistance, 1.0);
+       double spinnerDistance = 60;
+        //sleep();
 
-        spinner.setPower(1);
+        mecanum.encoderDrive(speed,spinnerDistance,0,-spinnerDistance,-spinnerDistance,0,spinnerDistance, 10.0);
+
+        // rotate anticlockwise
+        double rotateDistance = 10;
+        mecanum.encoderDrive(speed,-rotateDistance,0,-rotateDistance,rotateDistance,0,rotateDistance, 5.0);
+
+
+        spinner.setPower(-0.58);
+        sleep(1565);
 
         //come back to the regular position - turn left - 5 inches
-        mecanum.encoderDrive(speed,spinnerDistance,0,-spinnerDistance,-spinnerDistance,0,spinnerDistance, 1.0);
+     //   mecanum.encoderDrive(speed,spinnerDistance,0,-spinnerDistance,-spinnerDistance,0,spinnerDistance, 1.0);
 
         //Scan for position of the element
 
